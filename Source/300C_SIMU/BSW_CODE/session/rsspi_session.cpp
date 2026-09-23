2e1fa27439bf316ece50480724a16d6c1a29184b
syy <syy319@sina.com>
2026-09-23 17:29:09 +0800

    log rssp1 message

--- Source/300C_SIMU/BSW_CODE/session/rsspi_session.cpp
+++ Source/300C_SIMU/BSW_CODE/session/rsspi_session.cpp
@@ -5,8 +5,15 @@
 #include "../Pub_Comm/connection_factory.h"
 #include "../../ASW_300C/interface_p2a.h"
 #include "../../ASW_300C/Interface_Data.h"
+#include "../../logging/logger.h"
 
 namespace {
+
+Logger::Ptr get_rssp1_log()
+{
+	return Logger::get("rssp1");
+}
+
 }
 
 rsspi_session::rsspi_session(const nlohmann::json& config)
@@ -37,6 +44,11 @@ session_result rsspi_session::do_input() noexcept
 		const connection_result res = connection_->receive(buffer.data(), buffer.size(), &received_size);
 		if (res == connection_result::ok)
 		{
+			get_rssp1_log()->trace("%s-0x%08X in: len=%zu",
+				name().c_str(),
+				static_cast<unsigned int>(id_),
+				received_size);
+
 			if (CVC_TRUE != writePFMsg(RSSPI_MSG_C_TO_APP_DATA, APP_TYPE_C_RSSP1, id_, buffer.data(), static_cast<INT16U>(received_size)))
 			{
 				return session_result::forward_failed;
@@ -83,5 +95,11 @@ session_result rsspi_session::output(const unsigned char* buffer, std::size_t le
 		return session_result::invalid_argument;
 	}
 
-	return session_map_send_result(connection_->send(buffer, len));
+	const session_result send_result = session_map_send_result(connection_->send(buffer, len));
+	get_rssp1_log()->trace("%s-0x%08X out: len=%zu result=%d",
+		name().c_str(),
+		static_cast<unsigned int>(id_),
+		len,
+		static_cast<int>(send_result));
+	return send_result;
 }
